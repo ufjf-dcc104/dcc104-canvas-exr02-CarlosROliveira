dc611c27a3a9063c07deb9696f3dcf0224d775f0
Carlos Oliveira <carlos.rc.oliveira@gmail.com>
2017-03-29 20:34:28 -0300

    Adicionando colisão e perseguição

---
+++ Level.js
@@ -0,0 +1,41 @@
+function Level(){
+  this.sprites =[];
+  this.inimigos = 3;
+}
+
+Level.prototype.init = function(){
+  for (var i = 0; i < this.inimigos; i++) {
+    var inimigo = new Sprite();
+    inimigo.x = 120+20*i;
+    inimigo.y = 10;
+    inimigo.width = 10+i*5;
+    inimigo.height = 10+i*5;
+    this.sprites.push(inimigo);
+  }
+};
+
+Level.prototype.mover = function (dt){
+  for (var i = 0; i < this.sprites.length; i++) {
+    this.sprites[i].mover(dt);
+  }
+};
+
+Level.prototype.desenhar = function (ctx){
+  for (var i = 0; i < this.sprites.length; i++) {
+    this.sprites[i].desenhar(ctx);
+  }
+};
+
+Level.prototype.colidiuCom = function (alvo, resolveColisao){
+  for (var i = 0; i < this.sprites.length; i++) {
+    if(this.sprites[i].colodiuCom(alvo)){
+      resolveColisao(this.sprites[i], alvo);
+    }
+  }
+};
+
+Level.prototype.perseguir = function (alvo, dt) {
+  for (var i = 0; i < this.sprites.length; i++) {
+    this.sprites[i].perseguir(alvo, dt);
+  }
+};
--- Sprite.js
+++ Sprite.js
@@ -1,26 +1,43 @@
 function Sprite(){
+  this.g = 0;
   this.x = 0;
   this.y = 0;
   this.vx = 0;
   this.vy = 0;
   this.ax = 0;
   this.ay = 0;
+  this.width = 15;
+  this.height = 15;
   this.color = "blue";
 }
 
 Sprite.prototype.desenhar = function (ctx){
   ctx.fillStyle = this.color;
-  ctx.fillRect(this.x, this.y, 15, 15);
+  ctx.fillRect(this.x, this.y, this.width, this.height);
   ctx.strokeStyle = "black";
-  ctx.strokeRect(this.x, this.y, 15, 15);
+  ctx.strokeRect(this.x, this.y, this.width, this.height);
 }
 
 Sprite.prototype.mover = function (dt){
   //Aceleração
   this.vx = this.vx + this.ax*dt;
-  this.vy = this.vy + (this.ay+60)*dt;
+  this.vy = this.vy + (this.ay+this.g)*dt;
 
   //Velocidade
   this.x = this.x + this.vx*dt;
   this.y = this.y + this.vy*dt;
+}
+
+//Colisão retangular
+Sprite.prototype.colodiuCom = function(alvo){
+  if(this.x + this.width < alvo.x)return false;
+  if(this.x > alvo.x + this.width)return false;
+  if(this.y + this.height < alvo.y)return false;
+  if(this.y > alvo.y + this.height)return false;
+  return true;
+}
+
+Sprite.prototype.perseguir = function(alvo, dt){
+  this.ax = 10*dt*(alvo.x - this.x) - 0.05*this.vx;
+  this.ay = 10*dt*(alvo.y - this.y) - 0.05*this.vy;
 }
